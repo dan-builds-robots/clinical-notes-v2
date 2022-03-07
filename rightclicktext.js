8a8e04d07d79149c0d8a2f754ada568a8c7d0f2b
Daniel Ajayi <dajayi@mit.edu>
2022-03-07 05:43:32 -0500

    linked the right click to the website that's been created

--- rightclicktext.js
+++ rightclicktext.js
@@ -16,16 +16,16 @@ chrome.contextMenus.onClicked.addListener(function(clickData) {
     if (clickData.menuItemId == "clinicalnotes") {
         console.log(clickData);
         console.log("Made a search");
-        var wikiUrl = "https://en.wikipedia.org/wiki/";
-        var createData = {
-            "url": wikiUrl,
-            "type": "popup",
-            "top": 5,
-            "left": 5,
+        var clinicalNotesURL = "https://dan-builds-robots.github.io/clinical-notes-website/";
+        // var createData = {
+        //     "url": wikiUrl,
+        //     "type": "popup",
+        //     "top": 5,
+        //     "left": 5,
             // "width": Math.round(window.screen.availWidth / 2),
             // "height": Math.round(window.screen.availHeight / 2)
-        };
+        // };
         // chrome.windows.create(createData, function(){});
-        chrome.tabs.create({ url: wikiUrl });
+        chrome.tabs.create({ url: clinicalNotesURL });
     }
 })
