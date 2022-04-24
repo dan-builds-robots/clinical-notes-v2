30f4a4671a065f6f8e01540416d90643baeb87dc
Niklas Mannhardt <niklasm@mit.edu>
2022-04-24 15:13:53 -0400

    integrated tokenizer

--- classification.js
+++ classification.js
@@ -2,10 +2,23 @@ const fs = require('fs');
 const util = require('util');
 const ort = require('onnxruntime-node');
 
+let { Tokenizer } = require("tokenizers/bindings/tokenizer")
+
 const InferenceSession = ort.InferenceSession;
 
 async function main() {
     try {
+
+        var test_sent = "This is a test sentence for tokenization";
+
+        let tokenizer = Tokenizer.fromFile('./tokenizer.json');
+        let encode = util.promisify(tokenizer.encode.bind(tokenizer));
+        // console.log('encode');
+        var output = await encode(test_sent);
+
+        console.log(output.getTokens());
+        console.log(output.getIds());
+        // console.log(tokenizer.encode(test_sent));
 
         var input = new ort.Tensor(
             type = 'float32',
@@ -13,15 +26,24 @@ async function main() {
         );
         // console.log(input);
 
-        feeds = {
-            input : input
+        bert_feeds = {
+            input_ids: output.getIds()
         };
 
-        session = await InferenceSession.create('./classifier.onnx');
-        
         const options = {
             logSeverityLevel: 0,
         };
+
+        bert_session = await InferenceSession.create('./bert.onnx');
+
+        const bert_result = await bert_session.run(bert_feeds);
+
+        feeds = {
+            input : input
+        };
+        
+        session = await InferenceSession.create('./classifier.onnx');
+        
         const result = await session.run(feeds, options);
         console.log(result);
 
