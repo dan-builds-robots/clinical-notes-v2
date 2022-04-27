22ca522301731942add084b46287aaa0492eff6b
Niklas Mannhardt <niklasm@mit.edu>
2022-04-27 13:54:08 -0400

    everything works in typescript

--- classification.ts
+++ classification.ts
@@ -1,100 +1,55 @@
-// const fs = require('fs');
-// const util = require('util');
-// const ort = require('onnxruntime-node');
+import { Tensor, InferenceSession } from 'onnxruntime-node';
+import { Tokenizer } from './Prediction/Tokenizer';
 
-import util from 'util';
-import { Collator } from './Prediction/Collator';
-// import { Tokenizer, InputSequenc } from 'tokenizers/bindings/tokenizer';
-import { Tokenizer } from './Prediction/Tokenizer.js';
-// let temp = require('./popup');
-// let { Collator } = require('./modules/Collator.ts');
-
-// let { Tokenizer } = require("tokenizers/bindings/tokenizer");
-
-// const InferenceSession = ort.InferenceSession;
-
+/**
+ * Sandbox for testing classes and functions.
+ * Use `ts-node.cmd .\classification.ts` to run
+ */
 async function main() {
-    try {
-
-        let split_sentence_input = [
-            "I have discussed with the patient in detail about the diagnosis of breast cancer and the surgical options, and medical oncologist has discussed with her issues about adjuvant or neoadjuvant chemotherapy.",
-            "We have decided to recommend to the patient breast conservation surgery with left breast lumpectomy with preoperative sentinel lymph node injection and mapping and left axillary dissection.",
-            "The possibility of further surgery requiring wider lumpectomy or even completion mastectomy was explained to the patient.",
-            "The procedure and risks of the surgery were explained to include, but not limited to extra bleeding, infection, unsightly scar formation, the possibility of local recurrence, the possibility of left upper extremity lymphedema was explained.",
-            "Local numbness, paresthesias or chronic pain was explained.",
-            "The patient was given an educational brochure and several brochures about the diagnosis and treatment of breast cancers.",
-            "She was certainly encouraged to obtain further surgical medical opinions prior to proceeding.",
-            "I believe the patient has given full informed consent and desires to proceed with the above."
-        ];
-        // console.log(split_sentence_input);
-
-        const col = new Collator();
+    try {            
+        // Load Tokenizer
         let tokenizer = Tokenizer.fromFile('./tokenizer.json');
 
-        // let t = Tokenizer.fromString("");
-        // let tokenizer = Tokenizer.fromFile("tokenizer.json");
+        var test_sent = "This is a test sentence for tokenization";
+        console.log("text before encoding:");
+        console.log(test_sent);
 
-        // let tokenizer = Tokenizer.fromFile('./tokenizer.json');
+        // encode test sentence
+        let output = await tokenizer.encode(test_sent);
+        console.log('Text after encoding');
+        console.log(output.getTokens());
+        console.log(output.getIds());
 
-        col.collate(
-            split_sentence_input,
-            tokenizer,
-            2
-        );
-        // var test_sent = "This is a test sentence for tokenization";
-        // let output = await tokenizer.encode(test_sent);
-        // console.log(output.getIds());
+        // Generate Random input
+        let nums = randomTensor(768);
+        var input = new Tensor('float32', nums);
+        console.log("Input");
+        console.log(input);
 
+        const options: InferenceSession.RunOptions = {
+            logSeverityLevel: 0,
+        };
 
-        // const encode = tokenizer.encode.bind(tokenizer);
-        // console.log('encode');
-        // let unencoded : InputSequence = test_sent;
-        // encode(test_sent, undefined, undefined);
-
-        // console.log('encoded');
-    //     // console.log(output.getTokens());
-        // console.log(output.getIds());
-    //     // console.log(Object.prototype.toString.call(output.getIds));
-    //     // // console.log(tokenizer.encode(test_sent));
-
-    //     // var input = new ort.Tensor(
-    //     //     type = 'float32',
-    //     //     data = randomTensor(768),
-    //     // );
-    //     // // console.log(input);
-
-    //     // bert_feeds = {
-    //     //     input_ids: output.getIds()
-    //     // };
-
-    //     // const options = {
-    //     //     logSeverityLevel: 0,
-    //     // };
-
-    //     // bert_session = await InferenceSession.create('./bert.onnx');
-
-    //     // const bert_result = await bert_session.run(bert_feeds);
-
-    //     // feeds = {
-    //     //     input : input
-    //     // };
+        let feeds = {
+            input : input
+        };
         
-    //     // session = await InferenceSession.create('./classifier.onnx');
+        // Create ONNX session
+        let session = await InferenceSession.create('./classifier.onnx');
         
-    //     // const result = await session.run(feeds, options);
-    //     // console.log(result);
+        // Run inference on random input
+        const result = await session.run(feeds, options);
+        console.log(result);
 
-    //     console.log('success');
+        console.log('success');
 
     } catch(e) {
         console.error(`inference failed: ${e}`);
     }
-    // console.log("this is a test");
 }
 
-// function randomTensor(size) {
-//         return Array.from({length: size}, () => Math.random());
-// }
+function randomTensor(size: number): number[] {
+        return Array.from({length: size}, () => Math.random());
+}
 
 main();
-
