5c7d0b08bc3c58762dbfeaa1cb32c1515fbf5ff2
Niklas Mannhardt <niklasm@mit.edu>
2022-04-26 23:07:26 -0400

    more fixes to make code runnable

--- classification.ts
+++ classification.ts
@@ -2,7 +2,10 @@
 // const util = require('util');
 // const ort = require('onnxruntime-node');
 
-import { Collator } from './modules/Collator.ts';
+import util from 'util';
+import { Collator } from './Prediction/Collator';
+// import { Tokenizer, InputSequenc } from 'tokenizers/bindings/tokenizer';
+import { Tokenizer } from './Prediction/Tokenizer.js';
 // let temp = require('./popup');
 // let { Collator } = require('./modules/Collator.ts');
 
@@ -13,7 +16,7 @@ import { Collator } from './modules/Collator.ts';
 async function main() {
     try {
 
-        split_sentence_input = [
+        let split_sentence_input = [
             "I have discussed with the patient in detail about the diagnosis of breast cancer and the surgical options, and medical oncologist has discussed with her issues about adjuvant or neoadjuvant chemotherapy.",
             "We have decided to recommend to the patient breast conservation surgery with left breast lumpectomy with preoperative sentinel lymph node injection and mapping and left axillary dissection.",
             "The possibility of further surgery requiring wider lumpectomy or even completion mastectomy was explained to the patient.",
@@ -23,65 +26,75 @@ async function main() {
             "She was certainly encouraged to obtain further surgical medical opinions prior to proceeding.",
             "I believe the patient has given full informed consent and desires to proceed with the above."
         ];
+        // console.log(split_sentence_input);
 
-        const col = new Collator()
-
+        const col = new Collator();
         let tokenizer = Tokenizer.fromFile('./tokenizer.json');
 
+        // let t = Tokenizer.fromString("");
+        // let tokenizer = Tokenizer.fromFile("tokenizer.json");
+
+        // let tokenizer = Tokenizer.fromFile('./tokenizer.json');
+
         col.collate(
-            sentences = split_sentence_input, 
-            tokenizer = tokenizer,
-            nContextSentences = 2,
+            split_sentence_input,
+            tokenizer,
+            2
         );
-
         // var test_sent = "This is a test sentence for tokenization";
-
-        // let encode = util.promisify(tokenizer.encode.bind(tokenizer));
-        // // console.log('encode');
-        // var output = await encode(test_sent);
-
-        // console.log(output.getTokens());
+        // let output = await tokenizer.encode(test_sent);
         // console.log(output.getIds());
-        // console.log(Object.prototype.toString.call(output.getIds));
-        // // console.log(tokenizer.encode(test_sent));
 
-        // var input = new ort.Tensor(
-        //     type = 'float32',
-        //     data = randomTensor(768),
-        // );
-        // // console.log(input);
 
-        // bert_feeds = {
-        //     input_ids: output.getIds()
-        // };
+        // const encode = tokenizer.encode.bind(tokenizer);
+        // console.log('encode');
+        // let unencoded : InputSequence = test_sent;
+        // encode(test_sent, undefined, undefined);
 
-        // const options = {
-        //     logSeverityLevel: 0,
-        // };
+        // console.log('encoded');
+    //     // console.log(output.getTokens());
+        // console.log(output.getIds());
+    //     // console.log(Object.prototype.toString.call(output.getIds));
+    //     // // console.log(tokenizer.encode(test_sent));
 
-        // bert_session = await InferenceSession.create('./bert.onnx');
+    //     // var input = new ort.Tensor(
+    //     //     type = 'float32',
+    //     //     data = randomTensor(768),
+    //     // );
+    //     // // console.log(input);
 
-        // const bert_result = await bert_session.run(bert_feeds);
+    //     // bert_feeds = {
+    //     //     input_ids: output.getIds()
+    //     // };
 
-        // feeds = {
-        //     input : input
-        // };
+    //     // const options = {
+    //     //     logSeverityLevel: 0,
+    //     // };
+
+    //     // bert_session = await InferenceSession.create('./bert.onnx');
+
+    //     // const bert_result = await bert_session.run(bert_feeds);
+
+    //     // feeds = {
+    //     //     input : input
+    //     // };
         
-        // session = await InferenceSession.create('./classifier.onnx');
+    //     // session = await InferenceSession.create('./classifier.onnx');
         
-        // const result = await session.run(feeds, options);
-        // console.log(result);
+    //     // const result = await session.run(feeds, options);
+    //     // console.log(result);
 
-        console.log('success');
+    //     console.log('success');
 
     } catch(e) {
         console.error(`inference failed: ${e}`);
     }
+    // console.log("this is a test");
 }
 
-function randomTensor(size) {
-        return Array.from({length: size}, () => Math.random());
-}
+// function randomTensor(size) {
+//         return Array.from({length: size}, () => Math.random());
+// }
 
 main();
 
