5638c8f8f8b73b0f1fb9bbbcfbeb5d6687388585
Niklas Mannhardt <niklasm@mit.edu>
2022-05-04 15:28:23 -0400

    resolve conflict

--- classification.ts
+++ classification.ts
@@ -1,64 +1,11 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { Tensor, InferenceSession } from 'onnxruntime-node';
-import { Tokenizer } from './Prediction/Tokenizer';
-=======
-// const fs = require('fs');
-// const util = require('util');
-// const ort = require('onnxruntime-node');
-
-import util from 'util';
-import ort, { Tensor } from 'onnxruntime-node';
-=======
->>>>>>> refactor CLIPModel
 import { Collator } from './Prediction/Collator';
 import { Tokenizer } from './Prediction/Tokenizer.js';
-<<<<<<< HEAD
-import { CLIPModel } from './Prediction/CLIPModel';
-// let temp = require('./popup');
-// let { Collator } = require('./modules/Collator.ts');
-
-// let { Tokenizer } = require("tokenizers/bindings/tokenizer");
-
-// const InferenceSession = ort.InferenceSession;
->>>>>>> finish implement collator, add corrected typing to CLIPModel
-=======
->>>>>>> refactor CLIPModel
 
 /**
  * Sandbox for testing classes and functions.
  * Use `ts-node.cmd .\classification.ts` to run
  */
 async function main() {
-<<<<<<< HEAD
-    try {            
-        // Load Tokenizer
-        let tokenizer = Tokenizer.fromFile('./tokenizer.json');
-
-        var test_sent = "This is a test sentence for tokenization";
-        console.log("text before encoding:");
-        console.log(test_sent);
-
-        // encode test sentence
-        let output = await tokenizer.encode(test_sent);
-        console.log('Text after encoding');
-        console.log(output.getTokens());
-        console.log(output.getIds());
-
-        // Generate Random input
-        let nums = randomTensor(768);
-        var input = new Tensor('float32', nums);
-        console.log("Input");
-        console.log(input);
-
-        const options: InferenceSession.RunOptions = {
-            logSeverityLevel: 0,
-        };
-
-        let feeds = {
-            input : input
-        };
-=======
     try {
 
         let split_sentence_input = [
@@ -83,77 +30,9 @@ async function main() {
 
         console.log(inputs[0]);
 
-<<<<<<< HEAD
-        let clip = new CLIPModel(
-            './bert.onnx',
-            './classifier.onnx',
-        );
-
-        let out = await clip.forward(inputs[0])
-        console.log(out);
-
-        // var test_sent = "This is a test sentence for tokenization";
-        // let output = await tokenizer.encode(test_sent);
-        // console.log(output.getIds());
-        // console.log(output.getAttentionMask());
-        // console.log(output.getTypeIds());
-
-
-        // const encode = tokenizer.encode.bind(tokenizer);
-        // console.log('encode');
-        // let unencoded : InputSequence = test_sent;
-        // encode(test_sent, undefined, undefined);
-
-        // console.log('encoded');
-    //     // console.log(output.getTokens());
-        // console.log(output.getIds());
-    //     // console.log(Object.prototype.toString.call(output.getIds));
-    //     // // console.log(tokenizer.encode(test_sent));
-
-    //     // var input = new ort.Tensor(
-    //     //     type = 'float32',
-    //     //     data = randomTensor(768),
-    //     // );
-    //     // // console.log(input);
-
-    //     // bert_feeds = {
-    //     //     input_ids: output.getIds()
-    //     // };
-
-    //     // const options = {
-    //     //     logSeverityLevel: 0,
-    //     // };
-
-    //     // bert_session = await InferenceSession.create('./bert.onnx');
-
-    //     // const bert_result = await bert_session.run(bert_feeds);
-
-    //     // feeds = {
-    //     //     input : input
-    //     // };
->>>>>>> finish implement collator, add corrected typing to CLIPModel
-        
-        // Create ONNX session
-        let session = await InferenceSession.create('./classifier.onnx');
-        
-        // Run inference on random input
-        const result = await session.run(feeds, options);
-        console.log(result);
-
-        console.log('success');
-
-=======
->>>>>>> refactor CLIPModel
     } catch(e) {
         console.error(`inference failed: ${e}`);
     }
 }
 
-<<<<<<< HEAD
-function randomTensor(size: number): number[] {
-        return Array.from({length: size}, () => Math.random());
-}
-
-=======
->>>>>>> refactor CLIPModel
 main();
